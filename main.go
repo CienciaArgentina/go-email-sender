1411217bcea94b75bcfc469be473722a283f484d
Nico <nicolasecamilo@gmail.com>
2020-09-07 02:00:42 -0300

    Log level

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/CienciaArgentina/go-backend-commons/pkg/clog"
 	"github.com/CienciaArgentina/go-email-sender/emailsender"
 )
 
 func main() {
+	clog.SetLogLevel(clog.DebugLevel)
 	emailSenderController := emailsender.NewController()
 	emailsender.InitRouter(emailSenderController)
 }
